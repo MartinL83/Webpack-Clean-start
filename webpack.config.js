56527a3a3df10336a932ad5843c15a9a0b5f3ef0
Martin Lindgren <lindgren2d@gmail.com>
2017-12-03 17:11:12 +0100

    CSS & SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].[contenthash].css",
+    disable: process.env.NODE_ENV === "development"
+});
 
 module.exports = {
   entry: {
-		index: './src/index.js',
+		main: './src/index.js',
 	},
 	output: {
+		path: path.resolve(__dirname, 'dist'),
 		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, 'dist')
 	},
 	module: {
 	  rules: [
@@ -21,10 +27,26 @@ module.exports = {
 	          presets: ['@babel/preset-env']
 	        }
 	      }
-	    }
+			},
+			{
+				test: /\.scss$/,
+				use: extractSass.extract({
+						use: [{
+								loader: "css-loader"
+						}, {
+								loader: "sass-loader",
+								options: {
+									includePaths: [ path.resolve(__dirname, 'src/sass') ]
+								}
+						}],
+						// use style-loader in development
+						fallback: "style-loader"
+				})
+			}
 	  ]
 	},
 	plugins: [
+		extractSass,
 		new CleanWebpackPlugin(['dist']),
 		new HtmlWebpackPlugin({
 			title: 'Webpack HTML Output'
